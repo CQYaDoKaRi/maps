9bacde6b10b6e06220c36f8e45f032e25783cab6
CQYaDoKaRi <cqyadokari@gmail.com>
2021-01-19 02:44:03 +0900

    [scss] gulp-sass を導入し、css を生成、minify 処理を追加（index.css は index.scss に変更、app.ts の処理を gulp にあわせて修正）

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ const tsProject = ts.createProject("tsconfig.json");
 // js - minify
 const uglify = require("gulp-uglify");
 
+// css(scss, sass)
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+
  // src
 const app = require("./public/js/app");
 const oApp = new app.app();
@@ -25,13 +29,22 @@ const env = {
 	}
 	, js: {
 		path: "public/js"
-		, srcApp: "public/js/app.js"
+		, srcApp: ["public/js/app.js"]
 		, srcIndex: oApp.getJs()
 		, srcLib: oApp.getJsLib()
 		, minApp: "app.min.js"
 		, minIndex: "index.min.js"
 		, uglify: {
 		}
+	}
+	, css: {
+		path: "public/css"
+		, src: [
+			"src/scss/**/*.scss"
+		]
+		, srcIndex: oApp.getCss()
+		, srcLib: oApp.getCssLib()
+		, minIndex: "index.min.css"
 	}
 }
 
@@ -40,6 +53,9 @@ let fTSC = false;
 let fJsAppMinify = false;
 let fJsIndexMinify = false;
 let fJsLibMinify = false;
+let fScss = false;
+let fScssMinify = false;
+let fScssLibMinify = false;
 
 // typescript - comple
 function tsc() {
@@ -117,9 +133,71 @@ function jsLibMinify() {
 	;
 }
 
+// scss - comple
+function scss() {
+	if (!fScss) {
+		console.log("scss", env.css.src);
+		fScss = true;
+	}
+
+	return gulp
+		.src(env.css.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(env.css.path))
+	;
+}
+
+// scss - minify
+function scssMinify() {
+	if (!fScssMinify) {
+		console.log("scss-minify", env.css.srcIndex);
+		fScssMinify = true;
+	}
+
+	let src = env.css.srcIndex.map(
+		(css) => {
+			return env.rootPublic + "/" + css;
+		}
+	);
+
+	return gulp
+		.src(src)
+		.pipe(sass({ outputStyle: "compact" }).on("error", sass.logError))
+		.pipe(concat(env.css.minIndex))
+		.pipe(gulp.dest(env.css.path))
+	;
+}
+
+// scss - Lib - minify
+function scssLibMinify() {
+	if (!fScssLibMinify) {
+		console.log("scss-lib-minify", env.css.srcLib);
+		fScssLibMinify = true;
+	}
+
+	let src = env.css.srcLib.map(
+		(css) => {
+			return env.rootPublic + "/" + css;
+		}
+	);
+	src.push("./" + env.css.path + "/" + env.css.minIndex);
+
+	return gulp
+		.src(src)
+		.pipe(concat(env.css.minIndex))
+		.pipe(gulp.dest(env.css.path))
+	;
+}
+
 // task - typescript
 gulp.task("tsc", gulp.series(tsc));
 // task - javascript - minify
 gulp.task("jsmin", gulp.series(jsAppMinify, jsIndexMinify, jsLibMinify));
+// task - css
+gulp.task("scss", gulp.series(scss));
+// task - css - minify
+gulp.task("cssmin", gulp.series(scssMinify, scssLibMinify));
 // task - build
-gulp.task("build", gulp.series(tsc, jsAppMinify, jsIndexMinify, jsLibMinify));
+gulp.task("build", gulp.series(tsc, jsAppMinify, jsIndexMinify, jsLibMinify, scss, scssMinify, scssLibMinify));
