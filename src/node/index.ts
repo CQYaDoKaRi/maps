1f461c3083757ce8ebac8344e54c59b76c2e3b80
CQYaDoKaRi <cqyadokari@gmail.com>
2021-01-27 23:48:33 +0900

    [node-api-maps] deg2name：角度から方位名を返す API を追加

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { maps } from '../ts/maps';
 
 const app:express.Express = express();
 
@@ -29,6 +30,28 @@ router.get('/',
 			res.send(html.join(''));
 			res.end();
         }
+);
+
+router.get('/api/maps/deg2name',
+	(req:express.Request, res:express.Response) => {
+		const oMaps: maps = new maps();
+		
+		let name: string = "";
+		if (req.query.deg) {
+			const deg: number = +req.query.deg
+			if (!Number.isNaN(deg)) {
+				name = oMaps.deg2Name(deg);
+			}
+		}
+		
+		const data: {} = {
+			status : true
+			, name : name
+		}
+		
+		res.json(data);
+		res.end();
+	}
 );
 
 app.use(router);
