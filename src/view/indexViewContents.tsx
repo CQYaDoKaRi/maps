377517a1c944706f3041dffb366fe3ecded0aeae
CQYaDoKaRi <cqyadokari@gmail.com>
2021-01-27 23:00:21 +0900

    [pages-title] タイトルを一元管理し表示処理を見直し

--- src/view/indexViewContents.tsx
+++ src/view/indexViewContents.tsx
@@ -9,12 +9,12 @@ export class IndexViewContents extends React.Component {
 		return(
 			<>
 			<div id="Distance" className="contents">
-				■　２地点間の距離と角度を求め、その地点からの距離と角度から緯度経度を求める<br />
+				■　<span id="appDistanceTitle"></span><br />
 				<div>
 					<a href="http://www.gsi.go.jp/common/000195510.pdf" target="_blank">国土地理院：都道府県庁間の距離（2018/01/15）</a>
 				</div>
 				｜　と比較すると、<br />
-				↓　ヒュベニによる計算が国土地理の計算とほぼ一致します。<br />
+				↓　ヒュベニによる計算が国土地理の計算とほぼ一致<br />
 				<div className="contentsDistanceTable">
 					<div id="appDistance" className="contentsDistanceTableWidth"></div>
 				</div>
