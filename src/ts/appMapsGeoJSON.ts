67e5bb92f547120605bc5d9a7644af10867d3410
CQYaDoKaRi <cqyadokari@gmail.com>
2021-02-17 17:17:20 +0900

    [pages-mongo-api] appMapsGeoJSON.ts のコードの型やメソッドを整理

--- src/ts/appMapsGeoJSON.ts
+++ src/ts/appMapsGeoJSON.ts
@@ -1,8 +1,13 @@
-import {Feature, Geometry} from "geojson";
-import L from "leaflet";
+import {Feature, Geometry} from 'geojson';
+import L from 'leaflet';
+
+interface FeatureGeometry{
+	type: string
+	coordinates: any[]
+}
 
 export class appMapsGeoJSON {
-	private url: string = "";
+	private url: string = '';
 	private layer: L.GeoJSON | null = null;
 	private layerVisible: boolean = false;
 	private markers: L.Marker[] = [];
@@ -21,30 +26,33 @@ export class appMapsGeoJSON {
 	 * @returns Promise<void>
 	 */
 	private get(url: string): Promise<void> {
-		return new Promise<void>((resolve: (data: any) => void, reject: (reson: any) => void) => {
+		return new Promise<void>((resolve: (data: any) => void) => {
 			fetch(url,
 				{
-					method: "GET"
+					method: 'GET'
 				}
-			).then(res => {
-				if (res.status === 200) {
-					res.text().then(text => {
-						if (text.length > 0) {
-							resolve(JSON.parse(text));
-						}
-						else{
-							resolve({});
-						}
+			).then(
+				res => {
+					if (res.status === 200) {
+						res.text().then(
+							text => {
+								if (text.length > 0) {
+									resolve(JSON.parse(text));
+								}
+								else{
+									resolve({});
+								}
+							}
+						);
 					}
-					);
+					else {
+						resolve({});
+					}
 				}
-				else {
+			).catch(
+				() => {
 					resolve({});
 				}
-			}
-			).catch(error => {
-				resolve({});
-			}
 			);
 		}
 		);
@@ -60,93 +68,104 @@ export class appMapsGeoJSON {
 			this.layerVisible = true;
 		}
 		else {
-			this.get(this.url).then((data: any) => {
-				if (!data) {
-					return;
-				}
-				const options: L.GeoJSONOptions = {
-					style: {
-						color: "#000000"
-						, weight: 1
-						, opacity: 0.80
+			this.get(this.url).then(
+				(data: any) => {
+					if (!data) {
+						return;
 					}
-					, onEachFeature: (feature: Feature<Geometry, any>, layer: L.GeoJSON) => {
-						let pref: number = 0;
+					const options: L.GeoJSONOptions = {
+						style: {
+							color: '#000000'
+							, weight: 1
+							, opacity: 0.80
+						}
+						, onEachFeature: (feature: Feature<Geometry, any>, layer: L.GeoJSON) => {
+							if (!feature.properties) {
+								return;
+							}
 
-						// pref & prefCitry
-						if (feature.properties) {
+							let pref: number = 0;
+							// - pref
 							if (feature.properties.pref) {
 								pref = feature.properties.pref;
 							}
+							// - prefCity
 							else if (feature.properties.JCODE) {
 								pref = +feature.properties.JCODE.substring(0, 2);
 							}
-
-							let color: string = "";
-							// 北海道
-							if (pref === 1) {
-								color = "#68cbc6";
-							}
-							// 東北
-							else if (pref >= 2 && pref <= 7) {
-								color = "#81d6eb";
-							}
-							// 関東
-							else if (pref >= 8 && pref <= 14) {
-								color = "#7595ec";
-							}
-							// 北陸
-							else if (pref >= 15 && pref <= 18) {
-								color = "#af6ec2";
-							}
-							// 中部
-							else if (pref >= 19 && pref <= 23) {
-								color = "#da6ea2";
-							}
-							// 近畿
-							else if (pref >= 24 && pref <= 30) {
-								color = "#eea849";
-							}
-							// 中国
-							else if (pref >= 31 && pref <= 35) {
-								color = "#e7d31a";
-							}
-							// 四国
-							else if (pref >= 36 && pref <= 39) {
-								color = "#aed44b";
-							}
-							// 九州
-							else if (pref >= 40 && pref <= 46) {
-								color = "#6eb318";
-							}
-							// 沖縄
-							else if (pref === 47) {
-								color = "#2e9f5f";
-							}
-
-							if (color) {
-								layer.setStyle(
-									{ color: color }
-								);
-							}
+							layer.setStyle(
+								{ color: this.setStyleColor(pref) }
+							);
 
 							this.setAttr(oMap, feature, layer);
 						}
 					}
-				}
 
-				this.layer = L.geoJSON(data, options);
-				this.layer.addTo(oMap);
-				this.layerVisible = true;
-			});
+					this.layer = L.geoJSON(data, options);
+					this.layer.addTo(oMap);
+					this.layerVisible = true;
+				}
+			);
 		}
 	}
 
 	/**
+	 * 設定：Style：Color
+	 * @param pref 都道府県コード
+	 * @returns 色
+	 */
+	private setStyleColor(pref: number): string{
+		// 北海道
+		if (pref === 1) {
+			return '#68cbc6';
+		}
+		// 東北
+		else if (pref >= 2 && pref <= 7) {
+			return '#81d6eb';
+		}
+		// 関東
+		else if (pref >= 8 && pref <= 14) {
+			return '#7595ec';
+		}
+		// 北陸
+		else if (pref >= 15 && pref <= 18) {
+			return '#af6ec2';
+		}
+		// 中部
+		else if (pref >= 19 && pref <= 23) {
+			return '#da6ea2';
+		}
+		// 近畿
+		else if (pref >= 24 && pref <= 30) {
+			return '#eea849';
+		}
+		// 中国
+		else if (pref >= 31 && pref <= 35) {
+			return '#e7d31a';
+		}
+		// 四国
+		else if (pref >= 36 && pref <= 39) {
+			return '#aed44b';
+		}
+		// 九州
+		else if (pref >= 40 && pref <= 46) {
+			return '#6eb318';
+		}
+		// 沖縄
+		else if (pref === 47) {
+			return '#2e9f5f';
+		}
+		return '#000000';
+	}
+
+	/**
+	/**
 	 * 設定：属性
 	 * @param oMap leaflet
+	 * @param feature 属性情報
+	 * @param layer レイヤー
 	 */
-	public setAttr(oMap: L.Map, feature: any, layer: L.GeoJSON): void {
+	public setAttr(oMap: L.Map, feature: Feature<Geometry, any>, layer: L.GeoJSON): void {
 		if (!oMap) {
 			return;
 		}
@@ -160,13 +179,13 @@ export class appMapsGeoJSON {
 		}
 		else if (feature.properties.JCODE) {
 			tname = '市区町村';
-			const name_gun = feature.properties.GUN ? feature.properties.GUN : "";
-			const name_shikuchoson = feature.properties.SIKUCHOSON ? feature.properties.SIKUCHOSON : "";
+			const name_gun = feature.properties.GUN ? feature.properties.GUN : '';
+			const name_shikuchoson = feature.properties.SIKUCHOSON ? feature.properties.SIKUCHOSON : '';
 			name = feature.properties.KEN + name_gun + name_shikuchoson;
 			fMarker = true;
 		}
 		const id: string = 'l_popup_attr_' + Math.random();
-		name = `<div id="${id}">${name}</div>`;
+		name = `<div id='${id}'>${name}</div>`;
 		layer.bindPopup(
 			name
 			, {
@@ -175,7 +194,7 @@ export class appMapsGeoJSON {
 		).on('popupopen', (e: L.PopupEvent) => {
 			this.markerRemove();
 
-			this.attr(feature, layer).then((data: any) => {
+			this.attr(feature.geometry).then((data: any) => {
 				const o: HTMLElement | null = L.DomUtil.get(id);
 				if (o) {
 					let n: number = 0;
@@ -200,19 +219,58 @@ export class appMapsGeoJSON {
 	}
 
 	/**
-	 * マーカー：削除
+	 * 属性
+	 * @param geometry 空間情報
+	 * @returns Promise<void>
 	 */
-	public markerRemove(): void {
-		this.markers.map((marker: L.Marker) => {
-			marker.remove();
-		});
+	private attr(geometry: FeatureGeometry | any): Promise<void> {
+		const url: string = 'api/maps/mongo/postoffice/inpolygon';
+
+		const gcoordinates: any = this.attrGeometoryCoordinatesDuplicateDelete(geometry);
+
+		const params: URLSearchParams = new URLSearchParams();
+		params.append('gtype', geometry.type);
+		params.append('gcoordinates', JSON.stringify(gcoordinates));
+		params.append('n', '0');
+
+		return new Promise<void>((resolve: (data: any) => void, reject: (reson: any) => void) => {
+			fetch(url,
+				{
+					method: 'POST'
+					, body: params
+				}
+			).then(
+				res => {
+					if (res.status === 200) {
+						res.text().then(text => {
+							if (text.length > 0) {
+								resolve(JSON.parse(text));
+							}
+							else{
+								resolve([]);
+							}
+						}
+						);
+					}
+					else {
+						resolve([]);
+					}
+				}
+			).catch(
+				() => {
+					resolve([]);
+				}
+			);
+		}
+		);
 	}
 
 	/**
-	 * 空間情報：重複座標削除
+	 * 属性：空間情報 - 重複座標削除
 	 * @param geometry 空間情報
+	 * @returns 空間情報
 	 */
-	private geoCoordinatesDuplicateDelete(geometry: any): any{
+	private attrGeometoryCoordinatesDuplicateDelete(geometry: FeatureGeometry): any{
 		let gcoordinates: any = geometry.coordinates;
 		if (geometry.type === 'Polygon') {
 			gcoordinates[0] = gcoordinates[0].filter(function(e1: any, index1: number){
@@ -237,48 +295,12 @@ export class appMapsGeoJSON {
 	}
 
 	/**
-	 * 属性
-	 * @param url geojson
-	 * @returns Promise<void>
+	 * マーカー：削除
 	 */
-	private attr(feature: any, layer: L.GeoJSON): Promise<void> {
-		const url: string = 'api/maps/mongo/postoffice/inpolygon';
-
-		const gcoordinates: any = this.geoCoordinatesDuplicateDelete(feature.geometry);
-
-		const params: URLSearchParams = new URLSearchParams();
-		params.append('gtype', feature.geometry.type);
-		params.append('gcoordinates', JSON.stringify(gcoordinates));
-		params.append('n', '0');
-
-		return new Promise<void>((resolve: (data: any) => void, reject: (reson: any) => void) => {
-			fetch(url,
-				{
-					method: "POST"
-					, body: params
-				}
-			).then(res => {
-				if (res.status === 200) {
-					res.text().then(text => {
-						if (text.length > 0) {
-							resolve(JSON.parse(text));
-						}
-						else{
-							resolve([]);
-						}
-					}
-					);
-				}
-				else {
-					resolve([]);
-				}
-			}
-			).catch(error => {
-				resolve([]);
-			}
-			);
-		}
-		);
+	public markerRemove(): void {
+		this.markers.map((marker: L.Marker) => {
+			marker.remove();
+		});
 	}
 
 	/**
