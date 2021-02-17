168aa090f051370e519cfcf8cb9870cc1b84d2a6
CQYaDoKaRi <cqyadokari@gmail.com>
2021-02-17 22:22:30 +0900

    [log4js] log4js を使いログ出力するように変更

---
+++ src/node/log.ts
@@ -0,0 +1,66 @@
+import path from 'path';
+import log4js from 'log4js';
+
+export class log{
+	private logger: log4js.Logger;
+
+	/**
+	 * コンストラクター
+	 * @param name ログファイル名
+	 */
+	constructor(name: string){
+		log4js.configure({
+			appenders:
+				{
+					console: {
+						type: "console"
+						, level	: "all"
+						, layout: {
+							type: 'pattern'
+							, pattern: '[%d{yyyy-MM-dd hh:mm:ss.SSS}] [%p] %m',
+						}
+					}
+					, system: {
+						type: "file"
+						, filename: path.join(__dirname, `./../../logs/${name}.log`)
+						, layout: {
+							type: 'pattern'
+							, pattern: '[%d{yyyy-MM-dd hh:mm:ss.SSS}] [%p] %m',
+						}
+						// 5MB
+						, maxLogSize: 5242880
+						// 世代管理(古いファイルは gz で圧縮)
+						, backups: 10
+						, compress: true
+					}
+				}
+				,categories: {
+					default: {
+						appenders: [
+							'console'
+							, 'system'
+						]
+						, level: "all"
+					}
+				}
+		});
+
+		this.logger = log4js.getLogger();
+	}
+
+	/**
+	 * ログ：info
+	 * @param msg メッセージ
+	 */
+	public info(msg: string): void{
+		this.logger.info(msg);
+	}
+
+	/**
+	 * ログ：error
+	 * @param msg エラー
+	 */
+	public error(msg: string): void{
+		this.logger.error(msg);
+	}
+}
