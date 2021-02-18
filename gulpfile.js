58c7f1c0634cb842b3ecdd1f0384c7652103f9ff
CQYaDoKaRi <cqyadokari@gmail.com>
2021-02-19 02:31:42 +0900

    [prettier] js, json の設定ファイル に対して prettier を実行

--- gulpfile.js
+++ gulpfile.js
@@ -31,134 +31,106 @@ const uglify = require("gulp-uglify");
 const sourcemaps = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 
- // src
+// src
 const app = require("./dist/public/ts/app");
 const oApp = new app.app();
 oApp.include();
 
 // 環境設定
 const env = {
-	root: "./"
-	, rootPublic: "./public"
-	, src : {
-		dist : "./src"
-	}
- 	, babel : {
-		path: "./dist/public.babel"
-		, src: [
-			"./dist/public/**/*.js"
-		]
-	}
-	, webpack : {
-		src: "./dist/public.babel/view/index.js"
-		, dist: {
-			path: path.join(__dirname, "./public/js")
-			, fname: "index.min.js"
-			, fnameDev: "index.js"
-		}
-	}
-	, js: {
-		path: "./public/js"
-		, srcApp: ["./dist/public.babel/ts/app.js"]
-		, srcIndex: oApp.getJs()
-		, srcLib: oApp.getJsLib()
-		, minApp: "app.min.js"
-		, minIndex: "index.min.js"
-		, uglify: {
-		}
-	}
-	, css: {
-		path: "./public/css"
-		, src: [
-			"./src/scss/*.scss"
-		]
-		, srcIndex: oApp.getCss()
-		, srcLib: oApp.getCssLib()
-		, minIndex: "index.min.css"
-	}
-}
+	root: "./",
+	rootPublic: "./public",
+	src: {
+		dist: "./src",
+	},
+	babel: {
+		path: "./dist/public.babel",
+		src: ["./dist/public/**/*.js"],
+	},
+	webpack: {
+		src: "./dist/public.babel/view/index.js",
+		dist: {
+			path: path.join(__dirname, "./public/js"),
+			fname: "index.min.js",
+			fnameDev: "index.js",
+		},
+	},
+	js: {
+		path: "./public/js",
+		srcApp: ["./dist/public.babel/ts/app.js"],
+		srcIndex: oApp.getJs(),
+		srcLib: oApp.getJsLib(),
+		minApp: "app.min.js",
+		minIndex: "index.min.js",
+		uglify: {},
+	},
+	css: {
+		path: "./public/css",
+		src: ["./src/scss/*.scss"],
+		srcIndex: oApp.getCss(),
+		srcLib: oApp.getCssLib(),
+		minIndex: "index.min.css",
+	},
+};
 
 // prettier
 const formatter = () => {
-	return tsProjectLint.src()
-		.pipe(prettier(prettierConfig))
-		.pipe(gulp.dest(env.src.dist))
-	;
-}
+	return tsProjectLint.src().pipe(prettier(prettierConfig)).pipe(gulp.dest(env.src.dist));
+};
 
 // eslint
 const linter = () => {
-	return tsProjectLint.src()
-		.pipe(eslint(
-			{
-				useEslintrc: true
-			}
-			)
+	return tsProjectLint
+		.src()
+		.pipe(
+			eslint({
+				useEslintrc: true,
+			})
 		)
 		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
-	;
-}
+		.pipe(eslint.failAfterError());
+};
 
 // typescript - comple
 const tsc = () => {
-	return tsProject.src()
-		.pipe(tsProject())
-		.js.pipe(gulp.dest(tsProject.options.outDir))
-	;
-}
+	return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(tsProject.options.outDir));
+};
 
 // typescript - comple - for node
 const tscNode = () => {
-	return tsProjectNode.src()
-		.pipe(tsProjectNode())
-		.js.pipe(gulp.dest(tsProjectNode.options.outDir))
-	;
-}
+	return tsProjectNode.src().pipe(tsProjectNode()).js.pipe(gulp.dest(tsProjectNode.options.outDir));
+};
 
 // js(6) - js(5)
 const jsBabel = () => {
-	return (
-		gulp
+	return gulp
 		.src(env.babel.src)
 		.pipe(
-			babel(
-				{
-					presets: ["@babel/preset-env"]
-				}
-			)
+			babel({
+				presets: ["@babel/preset-env"],
+			})
 		)
-		.pipe(gulp.dest(env.babel.path))
-	);
-}
+		.pipe(gulp.dest(env.babel.path));
+};
 
 // js - webpack - prod - minify
 const jsWebpack = () => {
-	return webpackStream(webpackConfig, webpack)
-	.pipe(gulp.dest(env.js.path)
-	);
-}
+	return webpackStream(webpackConfig, webpack).pipe(gulp.dest(env.js.path));
+};
 
 // js - webpack - dev
 const jsWebpackDev = () => {
 	webpackConfig.mode = "development";
 	webpackConfig.output.filename = webpackConfig.output.filename.replace(".min", "");
-	return webpackStream(webpackConfig, webpack)
-	.pipe(gulp.dest(env.js.path)
-	);
-}
+	return webpackStream(webpackConfig, webpack).pipe(gulp.dest(env.js.path));
+};
 
 // JavaScript - app - minify
 const jsAppMinify = () => {
 	del("./" + env.js.path + "/" + env.js.minApp);
 
-	return gulp
-		.src(env.js.srcApp)
-		.pipe(uglify(env.js.uglify))
-		.pipe(concat(env.js.minApp))
-		.pipe(gulp.dest(env.js.path))
-	;
-}
+	return gulp.src(env.js.srcApp).pipe(uglify(env.js.uglify)).pipe(concat(env.js.minApp)).pipe(gulp.dest(env.js.path));
+};
 
 // scss - comple
 const scss = () => {
@@ -167,41 +139,31 @@ const scss = () => {
 		.pipe(sourcemaps.init())
 		.pipe(sass().on("error", sass.logError))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(env.css.path))
-	;
-}
+		.pipe(gulp.dest(env.css.path));
+};
 
 // scss - minify
 const scssMinify = () => {
-	let src = env.css.srcIndex.map(
-		(css) => {
-			return env.rootPublic + "/" + css;
-		}
-	);
+	let src = env.css.srcIndex.map((css) => {
+		return env.rootPublic + "/" + css;
+	});
 
 	return gulp
 		.src(src)
 		.pipe(sass({ outputStyle: "compact" }).on("error", sass.logError))
 		.pipe(concat(env.css.minIndex))
-		.pipe(gulp.dest(env.css.path))
-	;
-}
+		.pipe(gulp.dest(env.css.path));
+};
 
 // scss - Lib - minify
 const scssLibMinify = () => {
-	let src = env.css.srcLib.map(
-		(css) => {
-			return env.rootPublic + "/" + css;
-		}
-	);
+	let src = env.css.srcLib.map((css) => {
+		return env.rootPublic + "/" + css;
+	});
 	src.push("./" + env.css.path + "/" + env.css.minIndex);
 
-	return gulp
-		.src(src)
-		.pipe(concat(env.css.minIndex))
-		.pipe(gulp.dest(env.css.path))
-	;
-}
+	return gulp.src(src).pipe(concat(env.css.minIndex)).pipe(gulp.dest(env.css.path));
+};
 
 // task - typescript
 const task_tsc = gulp.parallel(tsc, tscNode);
@@ -227,14 +189,14 @@ exports.scss = task_scss;
 const task_build = gulp.series(
 	gulp.parallel(
 		gulp.series(
-			formatter
-			, linter
-			, gulp.parallel(tsc, tscNode)
-			, jsBabel
-			, gulp.parallel(jsWebpack, jsWebpackDev)
-			, jsAppMinify
-		)
-		, task_scss
+			formatter,
+			linter,
+			gulp.parallel(tsc, tscNode),
+			jsBabel,
+			gulp.parallel(jsWebpack, jsWebpackDev),
+			jsAppMinify
+		),
+		task_scss
 	)
 );
 exports.build = task_build;
@@ -242,34 +204,19 @@ exports.build = task_build;
 // task - watch - build
 const watch_build = () => {
 	// linter
-	gulp.watch(env.src.src)
-	.on("change"
-		, gulp.series(linter)
-	);
+	gulp.watch(env.src.src).on("change", gulp.series(linter));
 
 	// tsc + babel
 	// +
 	// webpack, js-app
-	gulp.watch(tsProject.config.include)
-	.on("change"
-		, gulp.series(
-			tsc
-			, jsBabel
-			, jsAppMinify
-			, gulp.parallel(jsWebpack, jsWebpackDev)
-		)
-	);
+	gulp
+		.watch(tsProject.config.include)
+		.on("change", gulp.series(tsc, jsBabel, jsAppMinify, gulp.parallel(jsWebpack, jsWebpackDev)));
 
 	// tsc for node
-	gulp.watch(tsProjectNode.config.include)
-	.on("change"
-		, gulp.series(tscNode)
-	);
+	gulp.watch(tsProjectNode.config.include).on("change", gulp.series(tscNode));
 
 	// scss
-	gulp.watch(env.css.src)
-	.on("change"
-		, task_scss
-	);
+	gulp.watch(env.css.src).on("change", task_scss);
 };
 exports.watch = watch_build;
