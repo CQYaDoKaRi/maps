d77f91d2ca85051a606074b3fb795eaccf068527
CQYaDoKaRi <cqyadokari@gmail.com>
2021-03-14 01:32:59 +0900

    [react-distance] ViewMapsDistance コンポーネントに距離と角度を計算して地図表示を実装

--- src/view/ViewMapsDistance.tsx
+++ src/view/ViewMapsDistance.tsx
@@ -1,5 +1,5 @@
 // npm install --save-dev react @types/react
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { maps, mapsLatLon } from "../ts/maps";
 import { mapsDataPrefCapital, mapsDataPrefCapitalItem } from "../ts/mapsDataPrefCapital";
 import { appMaps } from "../ts/appMaps";
@@ -18,42 +18,100 @@ type Props = {
  * @param props props
  */
 const ViewMapsDistance: React.FC<Props> = (props) => {
-	const oMapsDataPrefCapital: mapsDataPrefCapital = new mapsDataPrefCapital();
-	const dmapsDataPrefCapital: mapsDataPrefCapitalItem[] = oMapsDataPrefCapital.get();
+	// 設定：都道府県番号
+	const vPrefN = 12;
 
+	// オブジェクト
+	const oMap = useRef(null);
+
+	// 地図
 	const oMaps: maps = new maps();
 
-	const _MapLat = props.mapLat;
-	const _MapLon = props.mapLon;
-	const _MapZ = props.mapZ;
-	const _MapOptions = {
-		w: 100,
-		wUnit: "%",
-		h: 600,
-		hUnit: "px",
+	// 地図データ：都道府県庁
+	const oMapsDataPrefCapital: mapsDataPrefCapital = new mapsDataPrefCapital();
+	const dMapsDataPrefCapital: mapsDataPrefCapitalItem[] = oMapsDataPrefCapital.get();
+	const dMapsDataPrefCapitalBase = dMapsDataPrefCapital[vPrefN];
+
+	/**
+	 * 都道府県県庁データから距離と角度を算出
+	 * @param item 都道府県県庁データ
+	 * @returns 距離と角度を算出した都道府県県庁データ
+	 */
+	const dataPrefCapitalItem = (item: mapsDataPrefCapitalItem): mapsDataPrefCapitalItem => {
+		const c: mapsLatLon = oMaps.distanceTo(
+			dMapsDataPrefCapitalBase.lat,
+			dMapsDataPrefCapitalBase.lon,
+			item.a,
+			item.distH
+		);
+
+		return {
+			pref: item.pref,
+			addr: item.addr,
+			lat: item.lat,
+			lon: item.lon,
+			distT: oMaps.distanceT(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
+			distH: oMaps.distanceH(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
+			distS: oMaps.distanceS(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
+			a: oMaps.direction(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
+			c_lat: c.lat,
+			c_lon: c.lon,
+		};
 	};
 
-	const base = 12;
-	const item_base = dmapsDataPrefCapital[base];
+	useEffect(() => {
+		const _MapOptions = {
+			w: 100,
+			wUnit: "%",
+			h: 600,
+			hUnit: "px",
+		};
 
-	// FIXME:地図表示を実装する
-	const oAppMaps = new appMaps("appDistanceMap", _MapLat, _MapLon, _MapZ, _MapOptions);
-	oAppMaps.layerBase();
+		const oDiv = (oMap.current as unknown) as HTMLElement;
+		const oAppMaps = new appMaps(oDiv.id, props.mapLat, props.mapLon, props.mapZ, _MapOptions);
+		oAppMaps.layerBase();
+
+		dMapsDataPrefCapital.map((item: mapsDataPrefCapitalItem) => {
+			item = dataPrefCapitalItem(item);
+
+			let options = {
+				color: "blue",
+				popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>緯度：${item.lat}</li><li>経度：${item.lon}</li></ol>`,
+			};
+			oAppMaps.point(item.lat, item.lon, options);
+
+			options = {
+				color: "green",
+				popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>${dMapsDataPrefCapitalBase.pref}から距離[${item.distH}m],方角[${item.a}]で求めた地点</li></ol>`,
+			};
+			oAppMaps.point(item.c_lat, item.c_lon, options);
+
+			const atob: number[][] = [];
+			atob.push([dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon]);
+			atob.push([item.lat, item.lon]);
+
+			options = {
+				color: "#4169e1",
+				popup: `<ol style="list-style-type:none;"><li>${dMapsDataPrefCapitalBase.pref}→${
+					item.pref
+				}</li><li>距離：${item.distH.toLocaleString()}</li><li>方角：${item.a}</li></ol>`,
+			};
+			oAppMaps.arc(atob, options);
+		});
+	}, []);
 
 	return (
-		<>
+		<div className="contentsDistance">
 			<div>
 				<a href="http://www.gsi.go.jp/common/000195510.pdf" target="_blank">
 					国土地理院：都道府県庁間の距離（2018/01/15）
 				</a>
 			</div>
-			｜&emsp;と比較すると、
-			<br />
-			↓&emsp;ヒュベニによる計算が国土地理の計算とほぼ一致
+			↓ &emsp;と比較すると、ヒュベニによる計算が国土地理の計算とほぼ一致
 			<br />
 			<div className="contentsDistanceTable">
 				<div id="appDistance" className="contentsDistanceTableWidth">
-					<div>{item_base.pref}からの距離</div>
+					<div>{dMapsDataPrefCapitalBase.pref}からの距離</div>
 					<dl>
 						<dt>県庁</dt>
 						<dd>緯度</dd>
@@ -89,46 +147,12 @@ const ViewMapsDistance: React.FC<Props> = (props) => {
 							（ヒュベニ）
 						</dd>
 					</dl>
-					{dmapsDataPrefCapital.map((item: mapsDataPrefCapitalItem, n: number) => {
-						if (n !== base) {
-							item.distT = oMaps.distanceT(item_base.lat, item_base.lon, item.lat, item.lon);
-							item.distH = oMaps.distanceH(item_base.lat, item_base.lon, item.lat, item.lon);
-							item.distS = oMaps.distanceS(item_base.lat, item_base.lon, item.lat, item.lon);
-							item.a = oMaps.direction(item_base.lat, item_base.lon, item.lat, item.lon);
-
-							const c: mapsLatLon = oMaps.distanceTo(item_base.lat, item_base.lon, item.a, item.distH);
-							item.c_lat = c.lat;
-							item.c_lon = c.lon;
-
-							// FIXME:地図表示を実装する
-							if (oAppMaps) {
-								let options = {
-									color: "blue",
-									popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>緯度：${item.lat}</li><li>経度：${item.lon}</li></ol>`,
-								};
-								oAppMaps.point(item.lat, item.lon, options);
-
-								options = {
-									color: "green",
-									popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>${item_base.pref}から距離[${item.distH}m],方角[${item.a}]で求めた地点</li></ol>`,
-								};
-								oAppMaps.point(item.c_lat, item.c_lon, options);
-
-								const atob: number[][] = [];
-								atob.push([item_base.lat, item_base.lon]);
-								atob.push([item.lat, item.lon]);
-
-								options = {
-									color: "#4169e1",
-									popup: `<ol style="list-style-type:none;"><li>${item_base.pref}→${
-										item.pref
-									}</li><li>距離：${item.distH.toLocaleString()}</li><li>方角：${item.a}</li></ol>`,
-								};
-								oAppMaps.arc(atob, options);
-							}
+					{dMapsDataPrefCapital.map((item: mapsDataPrefCapitalItem, n: number) => {
+						if (n !== vPrefN) {
+							item = dataPrefCapitalItem(item);
 
 							return (
-								<dl>
+								<dl key={n}>
 									<dt>{item.pref}</dt>
 									<dd>{item.lat}</dd>
 									<dd>{item.lon}</dd>
@@ -144,8 +168,8 @@ const ViewMapsDistance: React.FC<Props> = (props) => {
 					})}
 				</div>
 			</div>
-			<div id="appDistanceMap"></div>
-		</>
+			<div ref={oMap} id="appDistanceMap"></div>
+		</div>
 	);
 };
 
