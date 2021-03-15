c78bbf14753005ca18a99b916cd8067ed17fe50a
CQYaDoKaRi <cqyadokari@gmail.com>
2021-03-15 21:55:04 +0900

    [react-distance] 都道府県庁を選択して、その他の県庁との距離計算をできるように対応（地図表示の更新のみ未対応）

--- src/view/ViewMapsDistance.tsx
+++ src/view/ViewMapsDistance.tsx
@@ -1,5 +1,10 @@
 // npm install --save-dev react @types/react
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+// npm install --save-dev react-bootstrap bootstrap
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { maps, mapsLatLon } from "../ts/maps";
 import { mapsDataPrefCapital, mapsDataPrefCapitalItem } from "../ts/mapsDataPrefCapital";
 import { appMaps } from "../ts/appMaps";
@@ -18,19 +23,16 @@ type Props = {
  * @param props props
  */
 const ViewMapsDistance: React.FC<Props> = (props) => {
-	// 設定：都道府県番号
-	const vPrefN = 12;
+	// state - データ
+	const oMapsDataPrefCapital = new mapsDataPrefCapital();
+	const [dataPrefCapital] = useState<mapsDataPrefCapitalItem[]>(oMapsDataPrefCapital.get());
+	const [dataPrefCapitalIndex, setDataPrefCapitalIndex] = useState(12);
+	const [dataPrefCapitalBase, setDataPrefCapitalBase] = useState(dataPrefCapital[dataPrefCapitalIndex]);
+	// state - 地図
+	const [oAppMaps, setAppMaps] = useState<appMaps | undefined>(undefined);
 
 	// オブジェクト
 	const oMap = useRef(null);
-
-	// 地図
-	const oMaps: maps = new maps();
-
-	// 地図データ：都道府県庁
-	const oMapsDataPrefCapital: mapsDataPrefCapital = new mapsDataPrefCapital();
-	const dMapsDataPrefCapital: mapsDataPrefCapitalItem[] = oMapsDataPrefCapital.get();
-	const dMapsDataPrefCapitalBase = dMapsDataPrefCapital[vPrefN];
 
 	/**
 	 * 都道府県県庁データから距離と角度を算出
@@ -38,25 +40,36 @@ const ViewMapsDistance: React.FC<Props> = (props) => {
 	 * @returns 距離と角度を算出した都道府県県庁データ
 	 */
 	const dataPrefCapitalItem = (item: mapsDataPrefCapitalItem): mapsDataPrefCapitalItem => {
-		const c: mapsLatLon = oMaps.distanceTo(
-			dMapsDataPrefCapitalBase.lat,
-			dMapsDataPrefCapitalBase.lon,
-			item.a,
-			item.distH
-		);
+		const oMaps: maps = new maps();
+		const c: mapsLatLon = oMaps.distanceTo(dataPrefCapitalBase.lat, dataPrefCapitalBase.lon, item.a, item.distH);
 
 		return {
 			pref: item.pref,
 			addr: item.addr,
 			lat: item.lat,
 			lon: item.lon,
-			distT: oMaps.distanceT(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
-			distH: oMaps.distanceH(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
-			distS: oMaps.distanceS(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
-			a: oMaps.direction(dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon, item.lat, item.lon),
+			distT: oMaps.distanceT(dataPrefCapitalBase.lat, dataPrefCapitalBase.lon, item.lat, item.lon),
+			distH: oMaps.distanceH(dataPrefCapitalBase.lat, dataPrefCapitalBase.lon, item.lat, item.lon),
+			distS: oMaps.distanceS(dataPrefCapitalBase.lat, dataPrefCapitalBase.lon, item.lat, item.lon),
+			a: oMaps.direction(dataPrefCapitalBase.lat, dataPrefCapitalBase.lon, item.lat, item.lon),
 			c_lat: c.lat,
 			c_lon: c.lon,
 		};
+	};
+
+	/**
+	 * イベント：都道府県県庁の選択
+	 * @param index 都道府県県庁データインデックス
+	 */
+	const onChange = (index: number) => {
+		setDataPrefCapitalIndex(index);
+		setDataPrefCapitalBase(dataPrefCapital[index]);
+
+		// FIXME：地図更新処理を実装する
+		if (oAppMaps) {
+			//mapView(oAppMaps);
+			console.log("地図更新", `${index}===${dataPrefCapitalIndex}`);
+		}
 	};
 
 	useEffect(() => {
@@ -70,48 +83,77 @@ const ViewMapsDistance: React.FC<Props> = (props) => {
 		const oDiv = (oMap.current as unknown) as HTMLElement;
 		const oAppMaps = new appMaps(oDiv.id, props.mapLat, props.mapLon, props.mapZ, _MapOptions);
 		oAppMaps.layerBase();
+		setAppMaps(oAppMaps);
 
-		dMapsDataPrefCapital.map((item: mapsDataPrefCapitalItem) => {
-			item = dataPrefCapitalItem(item);
-
-			let options = {
-				color: "blue",
-				popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>緯度：${item.lat}</li><li>経度：${item.lon}</li></ol>`,
-			};
-			oAppMaps.point(item.lat, item.lon, options);
-
-			options = {
-				color: "green",
-				popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>${dMapsDataPrefCapitalBase.pref}から距離[${item.distH}m],方角[${item.a}]で求めた地点</li></ol>`,
-			};
-			oAppMaps.point(item.c_lat, item.c_lon, options);
-
-			const atob: number[][] = [];
-			atob.push([dMapsDataPrefCapitalBase.lat, dMapsDataPrefCapitalBase.lon]);
-			atob.push([item.lat, item.lon]);
-
-			options = {
-				color: "#4169e1",
-				popup: `<ol style="list-style-type:none;"><li>${dMapsDataPrefCapitalBase.pref}→${
-					item.pref
-				}</li><li>距離：${item.distH.toLocaleString()}</li><li>方角：${item.a}</li></ol>`,
-			};
-			oAppMaps.arc(atob, options);
-		});
+		mapView(oAppMaps);
 	}, []);
+
+	/**
+	 * 地図表示
+	 * @param oAppMaps leaflet
+	 */
+	const mapView = (oAppMaps: appMaps) => {
+		if (oAppMaps) {
+			dataPrefCapital.map((item: mapsDataPrefCapitalItem) => {
+				item = dataPrefCapitalItem(item);
+				let options = {
+					color: "blue",
+					popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>緯度：${item.lat}</li><li>経度：${item.lon}</li></ol>`,
+				};
+				oAppMaps.point(item.lat, item.lon, options);
+
+				options = {
+					color: "green",
+					popup: `<ol style="list-style-type:none;"><li>${item.pref}</li><li>${dataPrefCapitalBase.pref}から距離[${item.distH}m],方角[${item.a}]で求めた地点</li></ol>`,
+				};
+				oAppMaps.point(item.c_lat, item.c_lon, options);
+
+				const atob: number[][] = [];
+				atob.push([dataPrefCapitalBase.lat, dataPrefCapitalBase.lon]);
+				atob.push([item.lat, item.lon]);
+
+				options = {
+					color: "#4169e1",
+					popup: `<ol style="list-style-type:none;"><li>${dataPrefCapitalBase.pref}→${
+						item.pref
+					}</li><li>距離：${item.distH.toLocaleString()}</li><li>方角：${item.a}</li></ol>`,
+				};
+				oAppMaps.arc(atob, options);
+			});
+		}
+	};
 
 	return (
 		<div className="contentsDistance">
 			<div>
-				<a href="http://www.gsi.go.jp/common/000195510.pdf" target="_blank">
-					国土地理院：都道府県庁間の距離（2018/01/15）
-				</a>
+				<div style={{ float: "left" }}>
+					<DropdownButton title={dataPrefCapitalBase.pref}>
+						{dataPrefCapital.map((item: mapsDataPrefCapitalItem, n: number) => {
+							return (
+								<Dropdown.Item
+									key={n}
+									active={n === dataPrefCapitalIndex}
+									onSelect={() => {
+										onChange(n);
+									}}
+								>
+									{item.pref}
+								</Dropdown.Item>
+							);
+						})}
+					</DropdownButton>
+				</div>
+				<div style={{ paddingTop: "5px" }}>
+					&ensp;からの距離 （ヒュベニの計算結果が{" "}
+					<a href="http://www.gsi.go.jp/common/000195510.pdf" target="_blank">
+						国土地理院：都道府県庁間の距離（2018/01/15）
+					</a>{" "}
+					とほぼ一致）
+				</div>
 			</div>
-			↓ &emsp;と比較すると、ヒュベニによる計算が国土地理の計算とほぼ一致
-			<br />
+			<hr />
 			<div className="contentsDistanceTable">
 				<div id="appDistance" className="contentsDistanceTableWidth">
-					<div>{dMapsDataPrefCapitalBase.pref}からの距離</div>
 					<dl>
 						<dt>県庁</dt>
 						<dd>緯度</dd>
@@ -147,8 +189,8 @@ const ViewMapsDistance: React.FC<Props> = (props) => {
 							（ヒュベニ）
 						</dd>
 					</dl>
-					{dMapsDataPrefCapital.map((item: mapsDataPrefCapitalItem, n: number) => {
-						if (n !== vPrefN) {
+					{dataPrefCapital.map((item: mapsDataPrefCapitalItem, n: number) => {
+						if (n !== dataPrefCapitalIndex) {
 							item = dataPrefCapitalItem(item);
 
 							return (
