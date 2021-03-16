fafb6e045088cb45d10d4f73edda29abc5cebcc0
CQYaDoKaRi <cqyadokari@gmail.com>
2021-03-17 00:22:52 +0900

    [react-tile] ViewMapsTile コンポーネント化した部分の処理を index.ts から削除

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,6 +1,4 @@
-import { maps, mapsLatLon, mapsTile, mapsTileDem } from "../ts/maps";
 import { appMaps } from "../ts/appMaps";
-import { appMapsGSI } from "../ts/appMapsGSI";
 import { indexView } from "./indexView";
 import { ViewMenuTitle } from "./ViewMenu";
 
@@ -9,18 +7,16 @@ import { ViewMenuTitle } from "./ViewMenu";
  * @param oView indexView
  */
 function page(oView: indexView): void {
-	const oMaps: maps = new maps();
 	let oAppMaps: appMaps | null = null;
-	const oappMapsGSI = new appMapsGSI(oMaps);
 
 	const vHash = getHash();
 	oView.display(vHash);
 
 	/*==============================================================================================*/
 	// 地図
-	let _MapLat = 35.681236;
-	let _MapLon = 139.767125;
-	let _MapZ = 5;
+	const _MapLat = 35.681236;
+	const _MapLon = 139.767125;
+	const _MapZ = 5;
 	const _MapOptions = {
 		w: 100,
 		wUnit: "%",
@@ -28,174 +24,6 @@ function page(oView: indexView): void {
 		hUnit: "px",
 	};
 
-	/*==============================================================================================*/
-	if (!oView.status("Tile", vHash)) {
-		_MapZ = 4;
-		_MapLat = 35.360771305;
-		_MapLon = 138.7273035;
-
-		let vUrl = "";
-
-		let oDiv: HTMLElement | null = document.getElementById("Tile");
-		if (oDiv) {
-			oDiv.style.width = "900px";
-		}
-
-		const oDivTitleSub: HTMLElement | null = document.getElementById("appTileTitleSub");
-		if (oDivTitleSub) {
-			oDivTitleSub.innerHTML = `富士山山頂の緯度[ ${_MapLat} ]、緯度 [ ${_MapLon} ] からタイルとタイル情報を計算`;
-		}
-
-		oDiv = document.getElementById("appTile");
-		if (!oDiv) {
-			return;
-		}
-
-		oappMapsGSI.setDiv(oDiv);
-
-		for (let i = _MapZ; i < 19; i++) {
-			const vTile: mapsTile = oMaps.tile(_MapLat, _MapLon, i);
-			let vTileLatLon: mapsLatLon | null = null;
-			vUrl = `https://cyberjapandata.gsi.go.jp/xyz/std/${vTile.z}/${vTile.x}/${vTile.y}.png`;
-
-			const oTable: HTMLElement = document.createElement("table");
-			oTable.style.width = "890px";
-
-			//
-			let oTableTr: HTMLElement = document.createElement("tr");
-
-			let oTableTd: HTMLElement = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "ズームレベル";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `${vTile.z}`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			//
-			oTableTr = document.createElement("tr");
-
-			oTableTd = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "タイル座標座標(X, Y)";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `${vTile.x}, ${vTile.y}`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			//
-			oTableTr = document.createElement("tr");
-
-			oTableTd = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "タイル左上からpixel 値(X, Y)";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `${vTile.px_x}, ${vTile.px_y}`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			//
-			vTileLatLon = oMaps.tile2LatLon(vTile.x, vTile.y, vTile.z);
-
-			oTableTr = document.createElement("tr");
-
-			oTableTd = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "タイル左上の緯度、経度";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `${vTileLatLon.lat}, ${vTileLatLon.lon}`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			oTableTr = document.createElement("tr");
-
-			oTableTd = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "標高（TXT形式）<br>標高タイル";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `<div id="appTileDem${vTile.z}Txt"></div>`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			//
-			oTableTr = document.createElement("tr");
-
-			oTableTd = document.createElement("th");
-			oTableTd.style.textAlign = "left";
-			oTableTd.innerHTML = "標高（PNG形式）<br>標高タイル";
-			oTableTr.append(oTableTd);
-
-			oTableTd = document.createElement("td");
-			oTableTd.innerHTML = `<div id="appTileDem${vTile.z}Png"></div>`;
-			oTableTr.append(oTableTd);
-
-			oTable.append(oTableTr);
-
-			const oImg: HTMLImageElement = document.createElement("img");
-			oappMapsGSI.setTile(oImg, vTile);
-
-			oImg.src = vUrl;
-			oImg.onload = () => {
-				oappMapsGSI.Symbol(24, 24);
-			};
-
-			oDiv.append(oTable);
-			oDiv.append(oImg);
-
-			if (oMaps) {
-				const oMapTileDem: Promise<mapsTileDem> | null = oMaps.tileDemTxt(vTile);
-				if (oMapTileDem) {
-					void oMapTileDem.then((data: mapsTileDem) => {
-						if (!data.tile) {
-							return;
-						}
-						const o: HTMLElement | null = document.getElementById(`appTileDem${data.tile.z}Txt`);
-						if (!o) {
-							return;
-						}
-						if (isNaN(data.e)) {
-							o.innerHTML = "標高データなし";
-						} else {
-							o.innerHTML = data.e.toLocaleString() + " m" + "<br>" + data.url;
-						}
-					});
-				}
-
-				const oMapTileDemPng: Promise<mapsTileDem> | null = oMaps.tileDemPng(vTile);
-				if (oMapTileDemPng) {
-					void oMapTileDemPng.then((data: mapsTileDem) => {
-						if (!data.tile) {
-							return;
-						}
-						const o: HTMLElement | null = document.getElementById(`appTileDem${data.tile.z}Png`);
-						if (!o) {
-							return;
-						}
-						if (isNaN(data.e)) {
-							o.innerHTML = "標高データなし";
-						} else {
-							o.innerHTML = data.e.toLocaleString() + " m" + "<br>" + data.url;
-						}
-					});
-				}
-			}
-		}
-	}
 	/*==============================================================================================*/
 	if (!oView.status("MongoDB", vHash)) {
 		oAppMaps = new appMaps("appMongoDBMap", _MapLat, _MapLon, _MapZ, _MapOptions);
